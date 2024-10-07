9d6b809e2828d2479a73a04f1613673ad3742e8b
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2024-10-08 02:05:15 +0900

    update app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,14 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+import type { User } from "$lib/server/user";
+import type { Session } from "$lib/server/session";
+
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
-			user: import("$lib/server/user").User | null;
-			session: import("$lib/server/session").Session | null;
+			user: User | null;
+			session: Session | null;
 		}
 		// interface PageData {}
 		// interface PageState {}
